23ec09d0408b363c7839ac45943cfe35f1141e50
im-satyam <157481814+im-satyam@users.noreply.github.com>
2024-10-21 03:01:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Board from "./TikTacToeGame/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
